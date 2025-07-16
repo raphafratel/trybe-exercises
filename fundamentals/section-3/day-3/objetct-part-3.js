a4ebc2fe7a86615687628181a5cd635327fe4976
Raphael Fratel <raphael.fratel@gmail.com>
2025-07-16 08:26:43 -0300

    Resolve exercícios da parte 3 dos exercicios do dia 3.3

--- fundamentals/section-3/day-3/objetct-part-3.js
+++ fundamentals/section-3/day-3/objetct-part-3.js
@@ -30,17 +30,58 @@ const school = {
     },
   ]
 };
-/*
+/* Exercício 1
 Crie uma função que obtenha o valor da chave de acordo com sua posição no array. Essa função deve possuir dois parâmetros: o objeto e a posição no array.
 */
 
-/*
+const getValueByIndex = (obj, index) => Object.values(obj)[index];
+
+console.log(getValueByIndex(school.lessons, 1));
+
+
+/* Exercício 2
 Crie uma função que retorne a soma do número total de estudantes em todos os cursos.
 */
 
-/*
+const getTotalStudents = (obj) => {
+  let totalStudents = 0;
+
+  for (let index = 0; index < obj.lessons.length; index += 1) {
+    totalStudents += obj.lessons[index].students;
+  }
+
+  return totalStudents;
+}
+
+console.log(getTotalStudents(school));
+
+
+/* Exercicio 03
 Crie uma função que verifica se uma determinada chave existe em todos os elementos do array lessons. O retorno deve ser um booleano (true ou false). Essa função deve possuir dois parâmetros: o objeto e o nome da chave.
 */
 
-/*Crie uma função para alterar o turno para noite no curso de Python. Essa função deve ter três parâmetros: a base de dados a ser modificada, o nome do curso e o novo valor da chave.
+const verifyKey = (obj, key) => {
+  for (let index = 0; index < obj.lessons.length; index++) {
+    if (obj.lessons[index][key] === undefined) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(verifyKey(school, 'professor'));
+
+/*
+Crie uma função para alterar o turno para noite no curso de Python. Essa função deve ter três parâmetros: a base de dados a ser modificada, o nome do curso e o novo valor da chave.
 */
+
+const changeShift = (obj, course, newShift) => {
+  for (let index = 0; index < obj.lessons.length; index += 1) {
+    if (obj.lessons[index]['course'] === course) {
+      obj.lessons[index].shift = newShift;
+    }
+  }
+}
+
+changeShift(school, 'Python', 'Noite');
+console.log(school);
